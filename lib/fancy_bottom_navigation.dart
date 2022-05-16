38d03a92b46fc2815aab89928fc9da54dd482c70
Chris Wales <qwales1@gmail.com>
2022-05-16 13:58:28 -0400

    fix Stack parameter

--- lib/fancy_bottom_navigation.dart
+++ lib/fancy_bottom_navigation.dart
@@ -5,14 +5,26 @@ import 'package:fancy_bottom_navigation/paint/half_clipper.dart';
 import 'package:fancy_bottom_navigation/paint/half_painter.dart';
 import 'package:flutter/material.dart';
 
-const double CIRCLE_SIZE = 60;
 const double ARC_HEIGHT = 70;
 const double ARC_WIDTH = 90;
-const double CIRCLE_OUTLINE = 10;
-const double SHADOW_ALLOWANCE = 20;
 const double BAR_HEIGHT = 60;
+const double CIRCLE_OUTLINE = 10;
+const double CIRCLE_SIZE = 60;
+const double SHADOW_ALLOWANCE = 20;
 
 class FancyBottomNavigation extends StatefulWidget {
+  final Function(int position) onTabChangedListener;
+
+  final Color circleColor;
+  final Color activeIconColor;
+  final Color inactiveIconColor;
+  final Color textColor;
+  final Gradient gradient;
+  final Color barBackgroundColor;
+  final List<TabData> tabs;
+  final int initialSelection;
+  final Key key;
+
   FancyBottomNavigation(
       {@required this.tabs,
       @required this.onTabChangedListener,
@@ -24,21 +36,8 @@ class FancyBottomNavigation extends StatefulWidget {
       this.textColor,
       this.gradient,
       this.barBackgroundColor})
-      : assert(onTabChangedListener != null),
-        assert(tabs != null),
+      : assert(tabs != null),
         assert(tabs.length > 1 && tabs.length < 5);
-
-  final Function(int position) onTabChangedListener;
-  final Color circleColor;
-  final Color activeIconColor;
-  final Color inactiveIconColor;
-  final Color textColor;
-  final Gradient gradient;
-  final Color barBackgroundColor;
-  final List<TabData> tabs;
-  final int initialSelection;
-
-  final Key key;
 
   @override
   FancyBottomNavigationState createState() => FancyBottomNavigationState();
@@ -62,66 +61,9 @@ class FancyBottomNavigationState extends State<FancyBottomNavigation>
   Color shadowColor;
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-
-    activeIcon = widget.tabs[currentSelected].iconData;
-
-    circleColor = (widget.circleColor == null)
-        ? (Theme.of(context).brightness == Brightness.dark)
-            ? Colors.white
-            : Theme.of(context).primaryColor
-        : widget.circleColor;
-
-    activeIconColor = (widget.activeIconColor == null)
-        ? (Theme.of(context).brightness == Brightness.dark)
-            ? Colors.black54
-            : Colors.white
-        : widget.activeIconColor;
-
-    barBackgroundColor = (widget.barBackgroundColor == null)
-        ? (Theme.of(context).brightness == Brightness.dark)
-            ? Color(0xFF212121)
-            : Colors.white
-        : widget.barBackgroundColor;
-    textColor = (widget.textColor == null)
-        ? (Theme.of(context).brightness == Brightness.dark)
-            ? Colors.white
-            : Colors.black54
-        : widget.textColor;
-    inactiveIconColor = (widget.inactiveIconColor == null)
-        ? (Theme.of(context).brightness == Brightness.dark)
-            ? Colors.white
-            : Theme.of(context).primaryColor
-        : widget.inactiveIconColor;
-    gradient = widget.gradient;
-    shadowColor = Theme.of(context).brightness == Brightness.dark
-        ? Colors.white54
-        : Colors.black12;
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _setSelected(widget.tabs[widget.initialSelection].key);
-  }
-
-  _setSelected(UniqueKey key) {
-    int selected = widget.tabs.indexWhere((tabData) => tabData.key == key);
-
-    if (mounted) {
-      setState(() {
-        currentSelected = selected;
-        _circleAlignX = -1 + (2 / (widget.tabs.length - 1) * selected);
-        nextIcon = widget.tabs[selected].iconData;
-      });
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Stack(
-      overflow: Overflow.visible,
+      clipBehavior: Clip.none,
       alignment: Alignment.bottomCenter,
       children: <Widget>[
         Container(
@@ -223,6 +165,61 @@ class FancyBottomNavigationState extends State<FancyBottomNavigation>
     );
   }
 
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    activeIcon = widget.tabs[currentSelected].iconData;
+
+    circleColor = (widget.circleColor == null)
+        ? (Theme.of(context).brightness == Brightness.dark)
+            ? Colors.white
+            : Theme.of(context).primaryColor
+        : widget.circleColor;
+
+    activeIconColor = (widget.activeIconColor == null)
+        ? (Theme.of(context).brightness == Brightness.dark)
+            ? Colors.black54
+            : Colors.white
+        : widget.activeIconColor;
+
+    barBackgroundColor = (widget.barBackgroundColor == null)
+        ? (Theme.of(context).brightness == Brightness.dark)
+            ? Color(0xFF212121)
+            : Colors.white
+        : widget.barBackgroundColor;
+    textColor = (widget.textColor == null)
+        ? (Theme.of(context).brightness == Brightness.dark)
+            ? Colors.white
+            : Colors.black54
+        : widget.textColor;
+    inactiveIconColor = (widget.inactiveIconColor == null)
+        ? (Theme.of(context).brightness == Brightness.dark)
+            ? Colors.white
+            : Theme.of(context).primaryColor
+        : widget.inactiveIconColor;
+    gradient = widget.gradient;
+    shadowColor = Theme.of(context).brightness == Brightness.dark
+        ? Colors.white54
+        : Colors.black12;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _setSelected(widget.tabs[widget.initialSelection].key);
+  }
+
+  void setPage(int page) {
+    widget.onTabChangedListener(page);
+    _setSelected(widget.tabs[page].key);
+    _initAnimationAndStart(_circleAlignX, 1);
+
+    setState(() {
+      currentSelected = page;
+    });
+  }
+
   _initAnimationAndStart(double from, double to) {
     _circleIconAlpha = 0;
 
@@ -239,22 +236,24 @@ class FancyBottomNavigationState extends State<FancyBottomNavigation>
     });
   }
 
-  void setPage(int page) {
-    widget.onTabChangedListener(page);
-    _setSelected(widget.tabs[page].key);
-    _initAnimationAndStart(_circleAlignX, 1);
+  _setSelected(UniqueKey key) {
+    int selected = widget.tabs.indexWhere((tabData) => tabData.key == key);
 
-    setState(() {
-      currentSelected = page;
-    });
+    if (mounted) {
+      setState(() {
+        currentSelected = selected;
+        _circleAlignX = -1 + (2 / (widget.tabs.length - 1) * selected);
+        nextIcon = widget.tabs[selected].iconData;
+      });
+    }
   }
 }
 
 class TabData {
-  TabData({@required this.iconData, @required this.title, this.onclick});
-
   IconData iconData;
+
   String title;
   Function onclick;
   final UniqueKey key = UniqueKey();
+  TabData({@required this.iconData, @required this.title, this.onclick});
 }
